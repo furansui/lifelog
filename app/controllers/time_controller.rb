f194cd38d1b36d7386fe1a8d1b5757cebaf41dd4
furansui <furansui@gmail.com>
2014-11-14 05:33:04 +0900

    fix timelog graph which passes midnight

--- app/controllers/time_controller.rb
+++ app/controllers/time_controller.rb
@@ -10,8 +10,6 @@ class TimeController < ApplicationController
     params[:begin] ||= (Time.zone.now-1.week).strftime('%Y-%m-%d 00:00')
     params[:end] ||= Time.zone.now.strftime('%Y-%m-%d 23:59')
 
-    #raise "#{params[:begin]}"
-
     @summary_begin = Time.zone.parse(params[:begin])
     @summary_end = Time.zone.parse(params[:end])
     @summary = Category.summarize(:begin => @summary_begin, :end => @summary_end)    
@@ -20,8 +18,8 @@ class TimeController < ApplicationController
 
   def timelogs
     if(params.has_key?(:time))
-      params[:begin] = params["time"]["begin(1i)"]+params["time"]["begin(2i)"]+params["time"]["begin(3i)"]+" 00:00"
-      params[:end] = params["time"]["end(1i)"]+params["time"]["end(2i)"]+params["time"]["end(3i)"]+" 23:59"
+      params[:begin] = params["time"]["begin(1i)"]+params["time"]["begin(2i)"].to_s.rjust(2, '0')+params["time"]["begin(3i)"].to_s.rjust(2, '0')+" 00:00"
+      params[:end] = params["time"]["end(1i)"]+params["time"]["end(2i)"].to_s.rjust(2, '0')+params["time"]["end(3i)"].to_s.rjust(2, '0')+" 23:59"
     end
     params[:begin] ||= (Time.zone.now-1.week).strftime('%Y-%m-%d 00:00')
     params[:end] ||= Time.zone.now.strftime('%Y-%m-%d 23:59')
--- app/models/timelog.rb
+++ app/models/timelog.rb
@@ -64,45 +64,64 @@ class Timelog < ActiveRecord::Base
     summary[:head][:total][:inseconds] = 0 #number of total duration
     summary[:head][:total][:days] = 0 #indicates number of days to show on graph
 
-    @sortedTimelog = Timelog.where(time: options[:begin]..options[:end]).order("time desc")
+    @sortedTimelog = Timelog.where(time: options[:begin]..options[:end]).order("time")
     if !@sortedTimelog.empty?            
       currentDay = @sortedTimelog.first.time.beginning_of_day;
       currentDayi = 1;
-      dayAccum = 0;
 
       summary[:row] = Array.new
-      @sortedTimelog.each_with_index do |timelog,index|        
-        thisrow = Hash.new
-        thisrow[:index] = index
-        thisrow[:color] = Category.find_by_id(timelog.category_id).root.color
-        thisrow[:timelog] = timelog
-        if index == 0
-          thisrow[:timelog].duration = (timelog.time.end_of_day - timelog.time).to_i
-        end
-        summary[:row] << thisrow
-
+      @sortedTimelog.each_with_index do |timelog,index|
         if timelog.time.beginning_of_day != currentDay
           currentDayi += 1
           currentDay = timelog.time.beginning_of_day
-          dayAccum = 0
         end
-        dayAccum += thisrow[:timelog].duration
-        thisrow[:dayaccum] = dayAccum
-        thisrow[:day] = currentDayi #indicates the day this record belong to on the graph        
-        #accumulative of this day
-        summary[:head][:total][:inseconds] += thisrow[:timelog].duration
+
+        thisrow = Hash.new
+        thisrow[:index] = index
+        thisrow[:color] = Category.find_by_id(timelog.category_id).root.color
+        thisrow[:duration] = timelog.duration
+        thisrow[:time] = timelog.time
+        thisrow[:event] = timelog.event
+        #if over the midnight, create new row
+        if (timelog.time + timelog.duration) > timelog.time.end_of_day
+          thisrow[:duration] = timelog.time.end_of_day - timelog.time          
+          thisrow[:dayaccum] = timelog.time - timelog.time.beginning_of_day
+          thisrow[:day] = currentDayi #indicates the day this record belong to on the graph        
+          summary[:row] << thisrow
+          
+          extrarow = Hash.new
+          extrarow[:index] = index
+          extrarow[:color] = Category.find_by_id(timelog.category_id).root.color
+          extrarow[:duration] = timelog.duration - thisrow[:duration]
+          extrarow[:time] = (timelog.time + 1.day).beginning_of_day
+          extrarow[:event] = timelog.event
+
+          extrarow[:dayaccum] = 0
+          currentDayi += 1
+          currentDay = (timelog.time + 1.day).beginning_of_day
+          extrarow[:day] = currentDayi #indicates the day this record belong to on the graph        
+          summary[:row] << extrarow
+
+          summary[:head][:total][:inseconds] += thisrow[:duration]
+          summary[:head][:total][:inseconds] += extrarow[:duration]
+        else
+          thisrow[:dayaccum] = timelog.time - timelog.time.beginning_of_day
+          thisrow[:day] = currentDayi #indicates the day this record belong to on the graph        
+          summary[:row] << thisrow
+          summary[:head][:total][:inseconds] += thisrow[:duration]
+        end
       end
 
-      summary[:head][:prev][:remaining] = @sortedTimelog.last.time - @sortedTimelog.last.time.midnight
+      summary[:head][:prev][:remaining] = @sortedTimelog.first.time - @sortedTimelog.first.time.midnight
       if summary[:head][:prev][:remaining] > 0
         @prevDayTimelog = Timelog.where('time < ?', options[:begin]).order("time desc")
         if !@prevDayTimelog.empty? 
           thisrow = Hash.new
-          thisrow[:timelog] = @prevDayTimelog.first
-          thisrow[:color] = Category.find_by_id(thisrow[:timelog].category_id).root.color
-          thisrow[:timelog].duration = summary[:head][:prev][:remaining]
-          thisrow[:timelog].time = @sortedTimelog.last.time.midnight
-          thisrow[:dayaccum] = 24*3600;
+          thisrow[:color] = Category.find_by_id(timelog.category_id).root.color
+          thisrow[:duration] = summary[:head][:prev][:remaining]
+          thisrow[:time] = @sortedTimelog.last.time.midnight
+          thisrow[:dayaccum] = 0
+          thisrow[:event] = timelog.event
           summary[:row] << thisrow
         end
       end
