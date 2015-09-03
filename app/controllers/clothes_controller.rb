6980ec62e724b3eb8e646bad08bc69b65edb086e
furansui <furansui@gmail.com>
2015-09-03 11:07:18 +0700

    add wear today button, add notice in clothes index, add path post for wear_today

--- app/controllers/clothes_controller.rb
+++ app/controllers/clothes_controller.rb
@@ -31,6 +31,14 @@ class ClothesController < ApplicationController
       render 'edit'
     end
   end
+
+  def wear_today
+    @clothe = Clothe.find(params[:format])
+    @clothe.wear << Date.today.strftime("%d %b %Y")
+    @clothe.save
+    flash[:notice] = "#{@clothe.name} is worn today."
+    redirect_to clothes_path
+  end
   
   private
   def clothe_params
--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,11 @@ Lifelog::Application.routes.draw do
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
-  resources :clothes
+  resources :clothes do
+    collection {
+      post :wear_today
+    }
+  end
   
   resources :categories do
     collection { 
