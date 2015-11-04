4818ddd41680a4bc629706566a785a74d928a6f1
furansui <furansui@gmail.com>
2015-11-05 03:05:02 +0900

    add multiple series for health (weight and poo) but still need to fix the missing data problem (weight go to zero or unconnected lines)

--- app/controllers/healths_controller.rb
+++ app/controllers/healths_controller.rb
@@ -1,7 +1,6 @@
 class HealthsController < ApplicationController
   def index
     @healths = Health.all
-    @weight = Health.weight
     respond_to do |format|
       format.html
       format.csv { send_data @healths.to_csv}
--- app/models/health.rb
+++ app/models/health.rb
@@ -1,10 +1,19 @@
 class Health < ActiveRecord::Base
-  def self.weight()
-    weight = Hash.new {|h,k| h[k] = 0}
-    weightCategory = HealthCategory.find_by name: 'weight'    
-    Health.where(health_category_id: weightCategory.id).each do |health|
-      weight[health.logged_at] = health.value
+  def self.by_category(categoryName)
+    dateStart = Health.order("logged_at").first.logged_at
+    dateEnd = Health.order("logged_at").last.logged_at
+
+    # create a hash for each date
+    cat = (dateStart.to_date..dateEnd.to_date).each_with_object({}) do |date,hash|
+    hash[date] = 0
     end
-    weight
+
+    # cat = Hash.new { |h,k| h[k] = 0}  
+    
+    catid = HealthCategory.find_by(name: categoryName).id
+    Health.where(health_category_id: catid).each do |health|
+      cat[health.logged_at.to_date] = health.value
+    end
+    cat
   end
 end
